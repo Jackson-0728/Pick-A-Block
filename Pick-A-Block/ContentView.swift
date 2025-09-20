c4af40339137acc9ce3c6aa286e4ddc5b0486f12
Jackson Liu <crazyjackson0728@gmail.com>
2025-09-20 16:20:15 +1200

    Added title, new spinning wheel

--- Pick-A-Block/ContentView.swift
+++ Pick-A-Block/ContentView.swift
@@ -1,10 +1,33 @@
 import SwiftUI
 import FortuneWheel
 
-struct ContentView: View {
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        let scanner = Scanner(string: hex)
+        var color: UInt64 = 0
+        scanner.scanHexInt64(&color)
 
-    private var players = ["Sameer", "Spikey", "Amelia", "Danny", "Karen", "Kathy"]
-    private var colors = ["#3F8BBD", "#20407A", "#CBB743", "#65588A", "#227D51", "#222", "#5B5B5B"]
+        let r = Double((color >> 16) & 0xFF) / 255.0
+        let g = Double((color >> 8) & 0xFF) / 255.0
+        let b = Double(color & 0xFF) / 255.0
+
+        self.init(red: r, green: g, blue: b)
+    }
+}
+
+struct ContentView: View {
+    
+    private let hexColors: [Color] = [
+        Color(hex: "3F8BBD"),
+        Color(hex: "20407A"),
+        Color(hex: "CBB743"),
+        Color(hex: "65588A"),
+        Color(hex: "227D51"),
+        Color(hex: "222222"),
+        Color(hex: "5B5B5B")
+    ]
+    private var colors = ["Light Blue", "Dark Blue", "Yellow", "Purple", "Green", "Black", "Grey"]
     
     var body: some View {
 //        let model = FortuneWheelModel(
@@ -13,11 +36,15 @@ struct ContentView: View {
 //            getWheelItemIndex: getWheelItemIndex
 //        )
         VStack{
+            Text("Pick-A-Block")
+                .font(.title)
+                .padding()
             ZStack {
-                FortuneWheel(titles: players, size: 320,
+                FortuneWheel(titles: colors, size: 320,
                              onSpinEnd: onSpinEnd,
+                             colors: hexColors)
+                
                              //                         getWheelItemIndex: getWheelItemIndex
-                )
             }
         }
     }
